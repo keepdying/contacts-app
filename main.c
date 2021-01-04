43b0b51bb20fff340a9bc2bab7bd24001f0ff5e2
KeepDying <yunusemre34tr@gmail.com>
2021-01-04 18:03:20 +0300

    test branch

--- main.c
+++ main.c
@@ -37,21 +37,44 @@ void menuFunc() {
 		menuFunc();
 	}
 }
+void addEntry(unsigned int count) {
+	entriesArray[count].id = count;
+	printf("\nName: \n");
+	scanf("%s", entriesArray[count].name);
+	printf("\nMail: \n");
+	scanf("%s", entriesArray[count].mail);
+	printf("\nPhone: \n");
+	scanf("%s", entriesArray[count].phone);
+}
+void listEntries(unsigned int count) {
+	for (i = 1; i < count; i++) {
 
+	}
+}
 struct entry // struct of dB
 {
 	unsigned int id;
 	char name[50];
 	char mail[50];
 	char phone[20];
+	time_t time;
+
 };
-struct entry entriesArray[999];
+struct entry entriesArray[999]; // max allowed entry
+unsigned int id_count; // this equals to entriesArray[0].id
 
 
-int main(void) {
+int main(int argc, char* argv[]) {
 	FILE* f = fopen("database.db", "r+b");
 	if (f == NULL) {
 		FILE* f = fopen("database.db", "w+b");
 	}
+	fread(entriesArray, sizeof(struct entry), sizeof(entriesArray), f);
+	id_count = entriesArray[0].id; // read user count
+
+	if (id_count == NULL) {
+		id_count = 0; // in case first run of program set id count to 0
+	}
+
 	menuFunc();
 };
